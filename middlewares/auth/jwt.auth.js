ba8bdcdd8c967650889ae16c44cb45a505c47280
Endah Sari <riri.mayries@gmail.com>
2022-07-20 21:52:22 +0700

    report attendance and create & update attendance

--- middlewares/auth/jwt.auth.js
+++ middlewares/auth/jwt.auth.js
@@ -7,7 +7,6 @@ const EmployeesDB = DB.employees;
 const verifyToken = (req, res, next) => {
   if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
     jwt.verify(req.headers.authorization.split(' ')[1], process.env.API_SECRET, function (err, decode) {
-      
 			if (err || !decode) {
 				req.user = undefined;
 				if(err) {
